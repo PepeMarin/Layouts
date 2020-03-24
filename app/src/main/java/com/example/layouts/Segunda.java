e069ceff7aba1b747821e94ec9052b34cffe35cc
PepeMarin <alberto_marin_isc@hotmail.com>
2020-03-24 01:59:31 -0600

    Intent

--- app/src/main/java/com/example/layouts/Segunda.java
+++ app/src/main/java/com/example/layouts/Segunda.java
@@ -2,7 +2,9 @@ package com.example.layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Segunda extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Segunda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("Dog");
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.tvTexto);
+        textView.setText(message);
     }
 }
