2ea2b7bbadadd1dbdfa832d42330efeb07b96947
PepeMarin <alberto_marin_isc@hotmail.com>
2020-03-24 01:56:14 -0600

    Intent

--- app/src/main/java/com/example/layouts/MainActivity.java
+++ app/src/main/java/com/example/layouts/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button bButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bButton1 = findViewById(R.id.button);
+        final Intent i= new Intent(this,Segunda.class);
+
+        bButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i.putExtra("Dog","Pepita");
+                startActivity(i);
+            }
+        });
     }
 }
